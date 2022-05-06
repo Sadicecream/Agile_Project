612dbc83292af08eb66ce4eb1d8ba83f44493872
Chris Earis <chrisearis414@gmail.com>
2022-05-05 21:34:07 -0700

    Added PUT method and fixed to work with Recipe and RecipeBook classes

--- app.py
+++ app.py
@@ -1,39 +1,37 @@
-from flask import Flask, request, json, url_for, render_template, redirect
+from flask import Flask, request, json, url_for, render_template, redirect, flash
+from models.recipe import Recipe
+from models.recipebook import RecipeBook
 import os
 app = Flask(__name__)
 
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 filename = os.path.join(SITE_ROOT,'data','data.json')
+
+
+collection = RecipeBook('collections')
+
+def writeJSON():
+    with open(filename,'w') as test_data:
+            data = []
+            for i in collection.recipes:
+                data.append(i.__dict__)
+            json.dump(data, test_data,
+                        indent=4,  
+                        separators=(',',': '))
    
 @app.route('/')
 def home():
-    if request.method == 'GET':
-        with open(filename,'r') as test_data:
-            data =json.load(test_data)
-
-        return render_template("index.html",recipes=data)
+    print(collection.recipes)
+    return render_template("index.html",recipes=collection.recipes)
 
 @app.route('/recipes/<recipe>',methods=['GET','DELETE'])
 def display(recipe):
     if request.method == 'GET':
-        with open(filename,'r') as test_data:
-            data =json.load(test_data)
-            for i in data:
-                if i["name"] == recipe:
-                    recipe_to_display = i
-                    return render_template("displayrecipe.html", display = recipe_to_display)
-            return redirect('/')
+        return render_template("displayrecipe.html", recipe =  collection.get_by_name(recipe))    
 
     if request.method == 'DELETE':
-        with open(filename,'r') as test_data:
-            data =json.load(test_data)
-            for i in data:
-                if i["name"] == recipe:
-                    data.remove(i)
-        with open(filename,'w') as test_data:
-            json.dump(data, test_data,
-                        indent=4,  
-                        separators=(',',': '))
+        collection.delete(recipe)
+        writeJSON()
         return redirect('/')
 
 @app.route('/create_recipe.html',methods=['GET','POST'])
@@ -42,19 +40,28 @@ def create():
         return render_template("create_recipe.html")
 
     if request.method == 'POST':
-        print(request.form)
-        new_entry = {
-            "instructions": str(request.form.getlist("Recipe Ingredients")[0]),
-            "ingredients": str(request.form.getlist("Recipe Ingredients")[0]),
-            "name": str(request.form.getlist("Recipe Name")[0])
-            }
-        with open(filename,'r') as test_data:
-            data = json.load(test_data)
-        data.append(new_entry)
-        with open(filename, 'w') as test_data:
-            json.dump(data, test_data,
-                        indent=4,  
-                        separators=(',',': '))
+        print(request.get_data())
+        instructions = str(request.form.getlist("Recipe Instructions")[0])
+        ingredients = str(request.form.getlist("Recipe Ingredients")[0])
+        name = str(request.form.getlist("Recipe Name")[0])
+        for i in collection.recipes:
+            if name == i.name:
+                flash('Recipe Name Already Exists','error')
+                return redirect('/')
+        new_entry = Recipe(name,ingredients,instructions)
+        collection.add(new_entry)
+        writeJSON()
+        return redirect('/')
+
+@app.route('/update/<recipe>',methods=['GET','PUT'])
+def update(recipe):
+    if request.method  == 'GET':
+        return render_template("updaterecipe.html", recipe = collection.get_by_name(recipe))
+
+    if request.method == 'PUT':
+        collection.get_by_name(recipe).instructions = str(request.form.getlist("Recipe Instructions")[0])
+        collection.get_by_name(recipe).ingredients = str(request.form.getlist("Recipe Ingredients")[0])
+        collection.get_by_name(recipe).name = str(request.form.getlist("Recipe Name")[0])
         return redirect('/')
 
 if __name__ == "__main__":
